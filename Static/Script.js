9057d3226fe51cf69be1c03a2d746d74c689c448
向晚 <xiangwan.guan@gmail.com>
2025-09-01 16:42:51 +0800

    deleted:    Static/Manifest.json 	modified:   Static/ModuleHelper.html 	modified:   Static/Script.js 	deleted:    Static/Serviceworker.js 	deleted:    Static/icon.png

--- Static/Script.js
+++ Static/Script.js
@@ -292,11 +292,5 @@
     );
   };
 
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("Serviceworker.js");
-    });
-  }
-
   main();
 })();
